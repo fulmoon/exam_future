5b075278c92197c328e946d16425f1a058622d1b
fulmoon <63829420+fulmoon@users.noreply.github.com>
2022-07-15 10:54:12 +0900

    Feat: add FloatingActionButton onMini

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> exam1() async {
-    var delay = Future.delayed(Duration(seconds: 3));
+    var delay = Future.delayed(const Duration(seconds: 3));
     delay.then((value) => print('Hello'));
   }
 }
@@ -57,7 +57,7 @@ class CounterApp extends StatefulWidget {
 }
 
 class _CounterAppState extends State<CounterApp> {
-  int count = 0;
+  int count = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -77,6 +77,15 @@ class _CounterAppState extends State<CounterApp> {
             style: const TextStyle(fontSize: 80),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('click');
+          setState(() {
+            count++;
+          });
+        },
       ),
     );
   }
